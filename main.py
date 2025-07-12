4368a4cb43ff13941ab22f55af7951948c41486c
maksympash-code <maksympash@knu.ua>
2025-07-12 15:19:40 +0300

    lesson_1_for_Programming_School

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600, 300))
+pygame.display.set_caption("Pygame SHAITAN Game")
+icon = pygame.image.load('images/icon.png')
+pygame.display.set_icon(icon)
+
+running = True
+while running:
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                screen.fill((3, 252, 69))
