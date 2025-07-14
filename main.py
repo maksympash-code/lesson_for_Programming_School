0700b3cb78b40fb8b6db2240778c44337b5126c0
maksympash-code <maksympash@knu.ua>
2025-07-14 13:54:10 +0300

    lesson_3_Adding_objects_to_the_screen

--- main.py
+++ main.py
@@ -6,14 +6,25 @@ pygame.display.set_caption("Pygame SHAITAN Game")
 icon = pygame.image.load('images/icon.png')
 pygame.display.set_icon(icon)
 
+myfont = pygame.font.Font('fonts/font.ttf', 40)
+text_logo = myfont.render('SHAITAN', False, 'Green')
+
+player = pygame.image.load('images/icon.png')
+
+square = pygame.Surface((50, 170))
+square.fill(('Blue'))
+
 running = True
 while running:
+    pygame.draw.circle(screen, 'Red', (70, 40), 30)
+    screen.blit(square, (50, 10))
+    screen.blit(text_logo, (150, 100))
+    screen.blit(player, (100, 50))
+
+
     pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                screen.fill((3, 252, 69))
