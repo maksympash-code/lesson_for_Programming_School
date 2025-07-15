a4f64172419030fa5ce3b969aa627befe7c5d530
maksympash-code <maksympash@knu.ua>
2025-07-15 14:12:48 +0300

    lesson_3_Object_animation_and_sounds

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
 
+
+clock = pygame.time.Clock()
 pygame.init()
-screen = pygame.display.set_mode((600, 300))
+screen = pygame.display.set_mode((618, 359))
 pygame.display.set_caption("Pygame SHAITAN Game")
 icon = pygame.image.load('images/icon.png')
 pygame.display.set_icon(icon)
@@ -9,17 +11,42 @@ pygame.display.set_icon(icon)
 myfont = pygame.font.Font('fonts/font.ttf', 40)
 text_logo = myfont.render('SHAITAN', False, 'Green')
 
-player = pygame.image.load('images/icon.png')
+background = pygame.image.load('images/background.png')
+background_sound = pygame.mixer.Sound('sounds/background_sound.mp3')
+background_sound.play()
 
-square = pygame.Surface((50, 170))
-square.fill(('Blue'))
+walk_left = [
+    pygame.image.load('images/player_left_1.png'),
+    pygame.image.load('images/player_left_2.png'),
+    pygame.image.load('images/player_left_3.png'),
+    pygame.image.load('images/player_left_4.png')
+]
+
+walk_right = [
+    pygame.image.load('images/player_right_1.png'),
+    pygame.image.load('images/player_right_2.png'),
+    pygame.image.load('images/player_right_3.png'),
+    pygame.image.load('images/player_right_4.png')
+]
+
+player_anim_count = 0
+background_x = 0
 
 running = True
 while running:
-    pygame.draw.circle(screen, 'Red', (70, 40), 30)
-    screen.blit(square, (50, 10))
-    screen.blit(text_logo, (150, 100))
-    screen.blit(player, (100, 50))
+    screen.blit(background, (background_x, 0))
+    screen.blit(background, (background_x + 618, 0))
+    screen.blit(walk_right[player_anim_count], (300, 250))
+
+    if player_anim_count == 3:
+        player_anim_count = 0
+    else:
+        player_anim_count += 1
+
+
+    background_x -= 2
+    if background_x == -618:
+        background_x = 0
 
 
     pygame.display.update()
@@ -28,3 +55,5 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+
+    clock.tick(15)
