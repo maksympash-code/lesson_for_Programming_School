5c4befe7ac591b37abf29899ab8b6b4eb77e401d
maksympash-code <maksympash@knu.ua>
2025-07-17 14:01:39 +0300

    lesson_6_Creating a Losing Screen

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ background_sound = pygame.mixer.Sound('sounds/background_sound.mp3')
 
 enemy = pygame.image.load('images/enemy.png').convert_alpha()
 enemy_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(enemy_timer, 2500)
+pygame.time.set_timer(enemy_timer, 2000)
 enemy_list_in_game = []
 
 walk_left = [
@@ -42,7 +42,13 @@ player_x = 150
 player_y = 250
 
 is_jump = False
-jump_count = 7
+jump_count = 8
+
+gamePlay = True
+label = pygame.font.Font('fonts/font.ttf', 40)
+lose_label = label.render('You lose!', False, (193, 196, 199))
+restart_label = label.render('Play again', False, (115, 132, 148))
+restart_label_rect = restart_label.get_rect(topleft = (225, 150))
 
 running = True
 while running:
@@ -50,55 +56,70 @@ while running:
     screen.blit(background, (background_x + 618, 0))
 
 
-    player_rect = walk_left[0].get_rect(topleft = (player_x, player_y))
+    if gamePlay:
+        player_rect = walk_left[0].get_rect(topleft = (player_x, player_y))
 
-    if enemy_list_in_game:
-        for el in enemy_list_in_game:
-            screen.blit(enemy, el)
-            el.x -= 10
+        if enemy_list_in_game:
+            for (i, el) in enumerate(enemy_list_in_game):
+                screen.blit(enemy, el)
+                el.x -= 10
 
-            if player_rect.colliderect(el):
-                print("You lose")
+                if el.x < -10:
+                    enemy_list_in_game.pop(i)
 
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        screen.blit(walk_left[player_anim_count], (player_x, player_y))
-    else:
-        screen.blit(walk_right[player_anim_count], (player_x, player_y))
+                if player_rect.colliderect(el):
+                    gamePlay = False
 
 
-    if keys[pygame.K_LEFT] and player_x > 50:
-        player_x -= player_speed
-    elif keys[pygame.K_RIGHT] and player_x < 200:
-        player_x += player_speed
-
-
-    if not is_jump:
-        if keys[pygame.K_SPACE]:
-            is_jump = True
-    else:
-        if jump_count >= -7:
-            if jump_count > 0:
-                player_y -= (jump_count ** 2) / 2
-            else:
-                player_y += (jump_count ** 2) / 2
-
-            jump_count -= 1
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            screen.blit(walk_left[player_anim_count], (player_x, player_y))
         else:
-            is_jump = False
-            jump_count = 7
+            screen.blit(walk_right[player_anim_count], (player_x, player_y))
 
 
-    if player_anim_count == 3:
-        player_anim_count = 0
+        if keys[pygame.K_LEFT] and player_x > 50:
+            player_x -= player_speed
+        elif keys[pygame.K_RIGHT] and player_x < 200:
+            player_x += player_speed
+
+
+        if not is_jump:
+            if keys[pygame.K_SPACE]:
+                is_jump = True
+        else:
+            if jump_count >= -8:
+                if jump_count > 0:
+                    player_y -= (jump_count ** 2) / 2
+                else:
+                    player_y += (jump_count ** 2) / 2
+
+                jump_count -= 1
+            else:
+                is_jump = False
+                jump_count = 8
+
+
+        if player_anim_count == 3:
+            player_anim_count = 0
+        else:
+            player_anim_count += 1
+
+
+        background_x -= 2
+        if background_x == -618:
+            background_x = 0
     else:
-        player_anim_count += 1
+        screen.fill((87, 88, 89))
+        screen.blit(lose_label, (230, 100))
+        screen.blit(restart_label, restart_label_rect)
 
+        mouse = pygame.mouse.get_pos()
+        if restart_label_rect.collidepoint(mouse) and pygame.mouse.get_pressed()[0]:
+            gamePlay = True
+            player_x = 150
+            enemy_list_in_game.clear()
 
-    background_x -= 2
-    if background_x == -618:
-        background_x = 0
 
     pygame.display.update()
 
